46a16c91c67ea97c1022aa5d7e14bfb1744c3e59
Ian Kamau <kian99564@gmail.com>
2024-05-18 01:19:25 +0300

    feat: admin can now upload image for product

--- db/migrate/20240517200736_create_products.rb
+++ db/migrate/20240517200736_create_products.rb
@@ -3,7 +3,7 @@ class CreateProducts < ActiveRecord::Migration[7.1]
     create_table :products do |t|
       t.string :name
       t.text :description
-      t.integer :quantity
+      t.integer :quantity, :default =>  0
       t.integer :price
 
       t.timestamps
